7c15eb361382a338de5fe7ce82d238077f3ebafc
刘新元#80800953 <80800953@yonghui.cn>
2019-09-27 22:41:57 +0800

    reconstruct

--- web/src/main/java/com/test/web/WebApplication.java
+++ web/src/main/java/com/test/web/WebApplication.java
@@ -1,19 +1,11 @@
 package com.test.web;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+
 import thread.CheckStatThread;
-import dao.DeviceDao;
-import dao.DeviceDaoImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import thread.SaleRecordThread;
-
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 
 @SpringBootApplication
